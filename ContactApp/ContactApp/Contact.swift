a28b9ef6c25fcad362ec9c09e04a102ec1352d9a
Moxit <shah.moxit999@gmail.com>
2022-04-07 16:35:44 -0400

    Delete ContactApp directory

--- ContactApp/ContactApp/Contact.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Contact.swift
-//  ContactApp
-//
-//  Created by Moxit Shah on 04/04/22.
-//
-
-import Foundation
--- ContactApp/ContactApp/ContactList.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ContactList.swift
-//  ContactApp
-//
-//  Created by Moxit Shah on 04/04/22.
-//
-
-import Foundation
